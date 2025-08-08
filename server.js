d569e0d6193f949fdb5aaaeb82cf468df35651c8
Rennifer Teixeira <rennifer10@gmail.com>
2025-08-07 23:18:35 -0300

    finalizando api

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { PrismaClient } from "./generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post("/usuarios", async (req, res) => {
   await prisma.user.create({
@@ -19,7 +21,19 @@ app.post("/usuarios", async (req, res) => {
 });
 
 app.get("/usuarios", async (req, res) => {
-  const users = await prisma.user.findMany();
+  let users = [];
+
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.query.age,
+      },
+    });
+  } else {
+    users = await prisma.user.findMany();
+  }
 
   res.status(200).json(users); //json com todos meus usuarios
 });
@@ -38,6 +52,16 @@ app.put("/usuarios/:id", async (req, res) => {
   });
 
   res.status(201).json(req.body); //retornando o usuario criado, alem de criar mostra o que foi criado
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id, // converte para número
+    },
+  });
+
+  res.status(200).json({ message: "Usuário deletado com sucesso!" });
 });
 
 app.listen(3000);
